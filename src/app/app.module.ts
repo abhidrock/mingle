5d86ad346901efed2eac71c759b197f4f8eb06a4
Abhishek Kumar Singh B <abhishek.b.kumar.singh@ericsson.com>
2018-04-13 13:31:10 +0530

    basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes, Router } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+
+const appRoutes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {
+    path: 'home', 
+    component: MainComponent,
+    children: [
+      
+    ]
+  }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
